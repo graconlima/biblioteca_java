94116b8e6614ff1d78ebc9299e9421e58f9831d0
eliatangracon <graconlima@yahoo.com.br>
2026-04-17 09:12:46 -0300

    Downgrade para 1.8

--- src/main/java/com/exemplo/biblioteca/model/Livro.java
+++ src/main/java/com/exemplo/biblioteca/model/Livro.java
@@ -1,6 +1,6 @@
 package com.exemplo.biblioteca.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 
 @Entity
 public class Livro {
--- src/test/java/com/example/biblioteca/BibliotecaApplicationTests.java
+++ src/test/java/com/example/biblioteca/BibliotecaApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.biblioteca;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test; // Import do JUnit 4
+import org.junit.runner.RunWith; // Import do JUnit 4
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner; // Runner para Java 8
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BibliotecaApplicationTests {
+public class BibliotecaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        // Teste de carga de contexto padrão
+    }
 
 }
